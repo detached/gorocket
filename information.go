3f6c937a9e2c949a96a1b2b8c08a2efb037ce22c
Simon Weis <simon.weis@1und1.de>
2017-01-13 17:23:04 +0100

    Extend server info function

--- information.go
+++ information.go
@@ -4,26 +4,62 @@ import (
 	"net/http"
 )
 
-type versionResponse struct {
-	Info Versions `json:"info"`
+type infoResponse struct {
+	Info Info `json:"info"`
 }
 
-type Versions struct {
+type Commit struct {
+	Hash string `json:"hash"`
+	Date string `json:"date"`
+	Author string `json:"author"`
+	Subject string `json:"subject"`
+	Tag string `json:"tag"`
+	Branch string `json:"branch"`
+}
+
+type Info struct {
+	Version string `json:"version"`
+	Build Build `json:"build"`
+	Travis Travis `json:"travis"`
+	Commit Commit `json:"commit"`
+	GraphicsMagick Library `json:"GraphicsMagick"`
+	ImageMagick Library `json:"ImageMagick"`
+}
+
+type Travis struct {
+	BuildNumber string `json:"buildNumber"`
+	Branch string `json:"branch"`
+	Tag string `json:"tag"`
+}
+
+type Build struct {
+	Date string `json:"date"`
+	NodeVersion string `json:"nodeVersion"`
+	Arch string `json:"arch"`
+	Platform string `json:"platform"`
+	OsRelease string `json:"osRelease"`
+	TotalMemory int64 `json:"totalMemory"`
+	FreeMemory int64 `json:"freeMemory"`
+	CpuCount int `json:"cpus"`
+}
+
+type Library struct {
+	Enabled bool `json:"enabled"`
 	Version string `json:"version"`
 }
 
-// Get version information from the server.
+// Get information about the server.
 // This function does not need a logged in user.
 //
 // https://rocket.chat/docs/developer-guides/rest-api/miscellaneous/info
-func (r *Rocket) GetVersions() (*Versions, error) {
+func (r *Rocket) GetServerInfo() (*Info, error) {
 	request, _ := http.NewRequest("GET", r.getUrl() + "/api/v1/info", nil)
 
-	response := new(versionResponse)
+	response := new(infoResponse)
 
 	if err := r.doRequest(request, response); err != nil {
 		return nil, err
 	}
 
-	return &response.Info, nil;
+	return &response.Info, nil
 }
--- information_test.go
+++ information_test.go
@@ -3,14 +3,39 @@ package gorocket
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"fmt"
 )
 
-func TestRocket_GetVersions(t *testing.T) {
+func TestRocket_GetServerInfo(t *testing.T) {
 	rocket := Rocket{Protocol:testProtocol, Host:testHost, Port:testPort}
 
-	versions, err := rocket.GetVersions()
+	info, err := rocket.GetServerInfo()
 
 	assert.Nil(t, err)
-	assert.NotNil(t, versions)
-	assert.NotEmpty(t, versions.Version)
+	assert.NotNil(t, info)
+
+	assert.NotEmpty(t, info.Version)
+
+	assert.NotEmpty(t, info.Build.Arch)
+	assert.NotZero(t, info.Build.CpuCount)
+	assert.NotEmpty(t, info.Build.Platform)
+	assert.NotEmpty(t, info.Build.Date)
+	assert.NotZero(t, info.Build.FreeMemory)
+	assert.NotZero(t, info.Build.TotalMemory)
+	assert.NotEmpty(t, info.Build.NodeVersion)
+	assert.NotEmpty(t, info.Build.OsRelease)
+
+	assert.NotEmpty(t, info.Travis.Branch)
+	assert.NotEmpty(t, info.Travis.BuildNumber)
+	assert.NotEmpty(t, info.Travis.Tag)
+
+	assert.NotEmpty(t, info.Commit.Author)
+	assert.NotEmpty(t, info.Commit.Branch)
+	assert.NotEmpty(t, info.Commit.Date)
+	assert.NotEmpty(t, info.Commit.Hash)
+	assert.NotEmpty(t, info.Commit.Subject)
+	assert.NotEmpty(t, info.Commit.Tag)
+
+	assert.NotEmpty(t, info.ImageMagick.Version)
+	assert.NotNil(t, info.GraphicsMagick)
 }
